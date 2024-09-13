6d0bd3f54ae9927353b747519a9be6dfdfe7d072
RoyLi511 <rrr111999o5@gmail.com>
2024-09-13 10:51:33 +0800

    新增依賴

---
+++ src/main/java/org/example/breakfastorderingpractice/BreakfastOrderingPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.breakfastorderingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastOrderingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakfastOrderingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/breakfastorderingpractice/BreakfastOrderingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.breakfastorderingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastOrderingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
